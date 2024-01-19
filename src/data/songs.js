07572111fee98ea34522e03d35fdff688a8a850f
Purush <selfpurushottam2006@gmail.com>
2024-01-20 00:42:00 +0530

    initial commit

--- src/data/songs.js
+++ src/data/songs.js
@@ -17,7 +17,7 @@ export default [
         description: 'Relax and indulge with beautiful piano pieces',
         image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
         type: 'album',
-        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/lyrical-kedarnath-sushant-rajput-sara-ali-khan-amit-trivedi-amitabh-b.mp3'
     },
     {
         id: 3,
@@ -26,7 +26,7 @@ export default [
         description: 'Artist',
         image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
         type: 'artist',
-        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/Samay%20Samjhayega%20Full%20Song%20%20Tum%20Prem%20Ho%20Sad%20%20Radha%20Krishn%20%20LOFI%20%20MOhit%20lalwani%20%20Surya%20Raj%20Kamal.mp3'
     },
     {
         id: 4,
@@ -35,7 +35,7 @@ export default [
         description: 'Original Soundtrack',
         image: 'https://i.scdn.co/image/d39397d2f6a1525b3fe90369c89ea2d94aac5714',
         type: 'podcast',
-        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/Ek%20Din%20Aap%20-%20HD%20VIDEO%20%20Shah%20Rukh%20Khan%20%26%20Juhi%20Chawla%20%20Yes%20Boss%20%2090s%20Romantic%20Hindi%20Songs.mp3'
     },
     {
         id: 5,
@@ -44,6 +44,6 @@ export default [
         description: 'Original Soundtrack',
         image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
         type: 'album',
-        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/Vilen%20-%20Kyun%20Dhunde%20(Official%20Video).mp3'
     }
 ];
