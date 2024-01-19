6093b56ea89038109624d3f5e1d0a1cdd5c68dad
Purush <selfpurushottam2006@gmail.com>
2024-01-19 22:47:38 +0530

    song activated

--- src/data/songs.js
+++ src/data/songs.js
@@ -6,45 +6,44 @@ export default [
         artist: 'Tayfun Abi',
         image: 'https://i.ytimg.com/vi/wncNcu6jEgs/hqdefault.jpg',
         type: 'album',
-        src: './Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
     },
     // Add more objects if needed
 
-
-	{
-		id: 2,
-		title: 'Peaceful Piano',
-		artist: 'Murat Abi',
-		description: 'Relax and indulge with beautiful piano pieces',
-		image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
-		type: 'album',
-		src: './Lists/raam-aayenge-swati-mishra-bhajan.mp3'
-	},
-	{
-		id: 3,
-		title: 'Hang Massive',
-		artist: 'Mert Abi',
-		description: 'Artist',
-		image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
-		type: 'artist',
-		src: 'https://freesound.org/data/previews/612/612087_1648170-lq.mp3'
-	},
-	{
-		id: 4,
-		title: 'Intouchables - Soundtracks OST',
-		artist: 'Gökhan Abi',
-		description: 'Original Soundtrack',
-		image: 'https://i.scdn.co/image/d39397d2f6a1525b3fe90369c89ea2d94aac5714',
-		type: 'podcast',
-		src: 'https://freesound.org/data/previews/612/612085_28867-lq.mp3'
-	},
-	{
-		id: 5,
-		title: 'Intouchables - Soundtracks OST',
-		artist: 'Mehmet Abi',
-		description: 'Original Soundtrack',
-		image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
-		type: 'album',
-		src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
-	}
-]
+    {
+        id: 2,
+        title: 'Peaceful Piano',
+        artist: 'Murat Abi',
+        description: 'Relax and indulge with beautiful piano pieces',
+        image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
+        type: 'album',
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+    },
+    {
+        id: 3,
+        title: 'Hang Massive',
+        artist: 'Mert Abi',
+        description: 'Artist',
+        image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
+        type: 'artist',
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+    },
+    {
+        id: 4,
+        title: 'Intouchables - Soundtracks OST',
+        artist: 'Gökhan Abi',
+        description: 'Original Soundtrack',
+        image: 'https://i.scdn.co/image/d39397d2f6a1525b3fe90369c89ea2d94aac5714',
+        type: 'podcast',
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+    },
+    {
+        id: 5,
+        title: 'Intouchables - Soundtracks OST',
+        artist: 'Mehmet Abi',
+        description: 'Original Soundtrack',
+        image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
+        type: 'album',
+        src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/raam-aayenge-swati-mishra-bhajan.mp3'
+    }
+];
