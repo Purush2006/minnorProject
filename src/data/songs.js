9061de3669f9336c40dd65046c156ab1a33fab7d
Purush <selfpurushottam2006@gmail.com>
2024-01-20 01:04:55 +0530

    initial commit

--- src/data/songs.js
+++ src/data/songs.js
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         title: 'Raam Aayenge || Swati Mishra Bhajan',
-        description: 'Original Soundtrack',
+        description: 'Swati Mishra | Ram Aayenge | राम आएंगे | Ram Mandir Ram Aayenge To Angana Sajaungi',
         artist: 'Swati Mishra',
         image: 'https://i.ytimg.com/vi/wncNcu6jEgs/hqdefault.jpg',
         type: 'album',
@@ -12,20 +12,20 @@ export default [
 
     {
         id: 2,
-        title: 'Peaceful Piano',
-        artist: 'Murat Abi',
-        description: 'Relax and indulge with beautiful piano pieces',
-        image: 'https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6',
+        title: 'Namo Namo (Kedarnath)',
+        artist: 'Amit Trivedi',
+        description: 'Lyrical | Kedarnath | Sushant Rajput | Sara Ali Khan | Amit Trivedi | Amitabh B Song: Namo Namo Singer: Amit Trivedi',
+        image: 'https://images.genius.com/99f5fe2e033c3c49b2009ec74b425437.300x300x1.jpg',
         type: 'album',
         src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/lyrical-kedarnath-sushant-rajput-sara-ali-khan-amit-trivedi-amitabh-b.mp3'
     },
     {
         id: 3,
-        title: 'Hang Massive',
-        artist: 'Mert Abi',
-        description: 'Artist',
-        image: 'https://i.scdn.co/image/ab67616100005174802686196d39eb0b7b5cd8b1',
-        type: 'artist',
+        title: 'Samay Samjhayega',
+        artist: 'Mohit Lalwani and Surya Raj Kamal',
+        description: 'Album, Radha krishn ; Singer, Mohit lalwani ; Lyrics, Shekhar Astitwa ; Music, Bharat kamal ;',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq7INMSPiywGNvQ-SyBB5w3r8KZWqttkSENQ&usqp=CAU',
+        type: 'album',
         src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/Samay%20Samjhayega%20Full%20Song%20%20Tum%20Prem%20Ho%20Sad%20%20Radha%20Krishn%20%20LOFI%20%20MOhit%20lalwani%20%20Surya%20Raj%20Kamal.mp3'
     },
     {
@@ -39,10 +39,10 @@ export default [
     },
     {
         id: 5,
-        title: 'Intouchables - Soundtracks OST',
-        artist: 'Mehmet Abi',
-        description: 'Original Soundtrack',
-        image: 'https://i.scdn.co/image/ab67706c0000da84fcb8b92f2615d3261b8eb146',
+        title: 'Kyun Dhunde hai tu bheed me ',
+        artist: 'Vilen',
+        description: ' Kyun Dhunde is a reminder that every moment is precious and should be cherished',
+        image: 'https://c.saavncdn.com/557/Kyun-Dhunde-Hindi-2023-20230418053251-500x500.jpg',
         type: 'album',
         src: 'https://raw.githubusercontent.com/Purush2006/minnorProject/main/src/data/Lists/Vilen%20-%20Kyun%20Dhunde%20(Official%20Video).mp3'
     }
